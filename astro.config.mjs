3b395502223c3f3d7f4c79d3ae2307032acf4a5c
chrostine <christineswierkosz@gmail.com>
2026-07-24 12:38:24 +0200

    big update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
